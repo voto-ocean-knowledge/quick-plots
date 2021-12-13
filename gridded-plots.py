04f2a897415b2e2b04afea122d7b26ad9b02f862
Callum Rollo <c.rollo@outlook.com>
2021-12-13 20:54:14 -0300

    create seperate maps

--- gridded-plots.py
+++ gridded-plots.py
@@ -20,6 +20,7 @@ from collections import defaultdict
 import boto3
 from botocore.exceptions import ClientError
 from matplotlib import style
+from PIL import Image
 
 style.use('presentation.mplstyle')
 _log = logging.getLogger(__name__)
@@ -315,6 +316,95 @@ def multiplotter(dataset, variables, plots_dir, glider='', mission='', grid=True
     return filename
 
 
+def scale_bar(ax, length=None, location=(0.5, 0.05), linewidth=3, coord=cartopy.crs.PlateCarree()):
+    """
+    ax is the axes to draw the scalebar on.
+    length is the length of the scalebar in km.
+    location is center of the scalebar in axis coordinates.
+    (ie. 0.5 is the middle of the plot)
+    linewidth is the thickness of the scalebar.
+    """
+    # Get the limits of the axis in lat long
+    llx0, llx1, lly0, lly1 = ax.get_extent(coord)
+    # Make tmc horizontally centred on the middle of the map,
+    # vertically at scale bar location
+    sbllx = (llx1 + llx0) / 2
+    sblly = lly0 + (lly1 - lly0) * location[1]
+    tmc = cartopy.crs.TransverseMercator(sbllx, sblly)
+    # Get the extent of the plotted area in coordinates in metres
+    x0, x1, y0, y1 = ax.get_extent(tmc)
+    # Turn the specified scalebar location into coordinates in metres
+    sbx = x0 + (x1 - x0) * location[0]
+    sby = y0 + (y1 - y0) * location[1]
+
+    # Calculate a scale bar length if none has been given
+    # (Theres probably a more pythonic way of rounding the number but this works)
+    if not length:
+        length = (x1 - x0) / 5000  # in km
+        ndim = int(np.floor(np.log10(length)))  # number of digits in number
+        length = round(length, -ndim)  # round to 1sf
+
+        # Returns numbers starting with the list
+        def scale_number(x):
+            if str(x)[0] in ['1', '2', '5']:
+                return int(x)
+            else:
+                return scale_number(x - 10 ** ndim)
+
+        length = scale_number(length)
+
+    # Generate the x coordinate for the ends of the scalebar
+    bar_xs = [sbx - length * 500, sbx + length * 500]
+    # Plot the scalebar
+    ax.plot(bar_xs, [sby, sby], transform=tmc, color='k', linewidth=linewidth)
+    # Plot the scalebar label
+    ax.text(sbx, sby, str(length) + ' km', transform=tmc,
+            horizontalalignment='center', verticalalignment='bottom')
+
+
+def make_map(nc, filename):
+    dataset = xr.open_dataset(nc)
+    lats = dataset.latitude.values
+    lons = dataset.longitude.values
+    times = dataset.time.values
+    coord = cartopy.crs.AzimuthalEquidistant(central_longitude=np.mean(lons),
+                                      central_latitude=np.mean(lats))
+    pc = cartopy.crs.PlateCarree()
+    fig = plt.figure(figsize=(12, 6))
+    ax = fig.add_subplot(111, projection=coord)
+    ax.scatter(lons, lats, transform=pc, s=10)
+    transform = cartopy.crs.PlateCarree()._as_mpl_transform(ax)
+
+    ax.annotate(f'{dataset.glider_name}\n{str(times[-1])[:10]}',
+                xy=(lons[-1], lats[-1]), xytext=(lons[-1]+1.2, lats[-1]+0.5),
+                arrowprops=dict(facecolor='black',
+                                arrowstyle="simple",
+                                alpha=0.5),
+                xycoords=transform,
+                ha='right', va='top')
+
+    lon_extend = 3
+    lat_extend = 1
+    lims = (np.nanmin(lons) - lon_extend, np.nanmax(lons) + lon_extend,
+            np.nanmin(lats) - lat_extend, np.nanmax(lats) + lat_extend)
+    ax.set_extent(lims, crs=pc)
+
+    feature = cartopy.feature.NaturalEarthFeature(name='land', category='physical',
+                                           scale='10m', edgecolor='black', facecolor='lightgreen')
+    ax.add_feature(feature)
+    gl = ax.gridlines(draw_labels=True,
+                      linewidth=2, color='gray', alpha=0.5, linestyle='--')
+    gl.top_labels = None
+    gl.right_labels = None
+    scale_bar(ax, location=(0.41, 0.05))
+
+    fn_root, fn_ext = str(filename).split('.')
+    filename_map = f"{fn_root}_map.{fn_ext}"
+    _log.info(f"writing mapt to {filename_map}")
+    fig.savefig(filename_map, format='png', transparent=True)
+    return filename_map
+
+
 def upload_to_s3(file_name, bucket, object_name=None, profile_name='voto:prod'):
     """Upload a file to an S3 bucket
     Original function by Isabelle Giddy
@@ -374,6 +464,8 @@ if __name__ == '__main__':
         grid = True
     _log.info(f'start plotting {netcdf} grid = {grid}')
     image_file = create_plots(netcdf, outdir, grid)
+    if 'map' in sys.argv:
+        map_file = make_map(netcdf, image_file)
     if 'upload' in sys.argv:
         path_parts = str(image_file).split('/')
         if grid:
@@ -382,3 +474,7 @@ if __name__ == '__main__':
             root_dir = 'nrt_mission_scatter'
         s3_filename = f'{root_dir}/{path_parts[-1]}'
         upload_to_s3(str(image_file), 'voto-figures', object_name=s3_filename, profile_name='produser')
+        if 'map' in sys.argv:
+            path_parts = str(map_file).split('/')
+            s3_filename = f'{root_dir}/{path_parts[-1]}'
+            upload_to_s3(str(map_file), 'voto-figures', object_name=s3_filename, profile_name='produser')

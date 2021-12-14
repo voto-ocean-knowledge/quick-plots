f57a4936f459359fcbe08e41a494008ccdae4f9a
Callum Rollo <c.rollo@outlook.com>
2021-12-14 19:38:02 -0300

    check modification times of files before processing

--- nrt_process.py
+++ nrt_process.py
@@ -31,7 +31,6 @@ def main():
     except FileNotFoundError:
         _log.warning("glider_locs.json not found. Making a new one")
         locs_dict_og = {}
-    locs_dict_new = locs_dict_og.copy()
     for i, row in to_process.iterrows():
         glider = str(row.glider)
         mission = str(row.mission)
@@ -41,9 +40,25 @@ def main():
         except IndexError:
             _log.error(f"nc file in {mission_dir} not found")
             continue
+        nc_time = nc_file.lstat().st_mtime
+        infile_time = 0
+        in_files = list(pathlib.Path(f'/data/raw-nrt/SEA0{glider}/0000{mission}/C-Csv/').glob('*'))
+        for file in in_files:
+            if file.lstat().st_mtime > infile_time:
+                infile_time = file.lstat().st_mtime
+        if nc_time > infile_time:
+            _log.info(f"No new SEA{glider} M{mission} files")
+            continue
         _log.info(f"Processing SEA{glider} M{mission}")
         ds = xr.open_dataset(nc_file)
         glider_locs_to_json(ds)
+
+    try:
+        with open(glider_locs_file, 'r') as openfile:
+            locs_dict_new = json.load(openfile)
+    except FileNotFoundError:
+        _log.warning("glider_locs.json not found.")
+        locs_dict_new = {}
     if locs_dict_new != locs_dict_og:
         upload_to_s3(glider_locs_file, 'voto-figures',
                      object_name='nrt_glider_locs.json', profile_name='produser', image=False)

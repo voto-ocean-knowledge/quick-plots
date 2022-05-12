59f862e60505f3a344300236f47d4d94406eec8d
Callum Rollo <c.rollo@outlook.com>
2022-05-12 16:42:22 +0200

    don't make prediction plot if voltage > 28

--- pilot_plots.py
+++ pilot_plots.py
@@ -41,21 +41,26 @@ def battery_plots(combined_nav_file, out_dir):
     end = datetime_pred[y_forward[:, 0] > 23][-1]
 
     fig, ax = plt.subplots(figsize=(12, 8))
-    ax.scatter(df_3day.index, df_3day.Voltage, label="Voltage last 3 days", s=3)
-    ax.plot(datetime_pred, y_forward, label="Linear prediction")
-    ax.set(xlim=(df_3day.index[0], end), ylim=(22.9, df_3day.Voltage.max() + 0.1))
-    v_per_ns = regr.coef_[0][0]
-    v_per_day = v_per_ns * 24 * 60 * 60 * 1e9
-    loss = np.round(np.abs(v_per_day), 2)
-    recover = datetime_pred[np.argmin(np.abs(24 - y_forward))]
-    recov_date = str(recover)[:10]
-    ax.text(0.05, 0.06, f"{loss} V/day\nrecover {recov_date}", transform=ax.transAxes)
-    ax.axhline(24, color="red")
-    ax.axvline(recover, color="red")
-    ax.grid()
-    ax.set(ylabel="Voltage (v)", title=title)
-    plt.xticks(rotation=45)
-    ax.legend(loc=1)
+    if (df_3day.Voltage > 28).any():
+        _log.info("voltage too low to make prediction plot")
+        ax.text(0.3, 0.5, "Voltage too high \nfor battery prediction", fontsize="24")
+        ax.axis("off")
+    else:
+        ax.scatter(df_3day.index, df_3day.Voltage, label="Voltage last 3 days", s=3)
+        ax.plot(datetime_pred, y_forward, label="Linear prediction")
+        ax.set(xlim=(df_3day.index[0], end), ylim=(22.9, df_3day.Voltage.max() + 0.1))
+        v_per_ns = regr.coef_[0][0]
+        v_per_day = v_per_ns * 24 * 60 * 60 * 1e9
+        loss = np.round(np.abs(v_per_day), 2)
+        recover = datetime_pred[np.argmin(np.abs(24 - y_forward))]
+        recov_date = str(recover)[:10]
+        ax.text(0.05, 0.06, f"{loss} V/day\nrecover {recov_date}", transform=ax.transAxes)
+        ax.axhline(24, color="red")
+        ax.axvline(recover, color="red")
+        ax.grid()
+        ax.set(ylabel="Voltage (v)", title=title)
+        plt.xticks(rotation=45)
+        ax.legend(loc=1)
     plt.tight_layout()
     filename = f"{out_dir}/battery_prediction.png"
     _log.info(f'writing figure to {filename}')

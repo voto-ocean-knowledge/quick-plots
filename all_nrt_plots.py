ebd088d2807851476844bd79b95fbde8477be83f
Callum Rollo <c.rollo@outlook.com>
2024-02-01 15:34:06 +0100

    all nrt plots with checks

--- all_nrt_plots.py
+++ all_nrt_plots.py
@@ -2,8 +2,17 @@ import pathlib
 from cmd_data_plots import command_cosole_log_plots
 from glidertools_plots import public_plots
 import matplotlib.pyplot as plt
+import logging
+_log = logging.getLogger(__name__)
+
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='/data/log/nrt_plots.log',
+                        filemode='a',
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        level=logging.INFO,
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    reprocess = False
     base_dir = '/data/data_l0_pyglider/nrt/'
     ts_files = list(pathlib.Path('/data/data_l0_pyglider/nrt/').rglob("mission_timeseries.nc"))
     for ts_file in ts_files:
@@ -11,8 +20,13 @@ if __name__ == '__main__':
         glider = int(parts[-4][3:])
         mission = int(parts[-3][1:])
         outdir = pathlib.Path(f"/data/plots/nrt/SEA{glider}/M{mission}/")
+        _log.info(f"process for SEA{glider}, M{mission}")
         if not outdir.exists():
             outdir.mkdir(parents=True)
-        public_plots(ts_file, outdir)
-        command_cosole_log_plots(glider, mission, outdir)
+        filename = outdir / f'SEA{glider}_M{mission}_gt.png'
+        if not filename.exists() or reprocess:
+            public_plots(ts_file, outdir)
+        filename = outdir / f'SEA{glider}_M{mission}_cmd_log.png'
+        if not filename.exists() or reprocess:
+            command_cosole_log_plots(glider, mission, outdir)
         plt.close('all')
